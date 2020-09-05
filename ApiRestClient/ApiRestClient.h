92abe5bd5b92d80e2b9c389b0e6a2d296d60df05
danielferrer <danybarco@gmail.com>
2020-09-05 08:21:39 +0200

    Primer commit

---
+++ ApiRestClient/ApiRestClient.h
@@ -0,0 +1,19 @@
+//
+//  ApiRestClient.h
+//  ApiRestClient
+//
+//  Created by Daniel Ferrer on 05/09/2020.
+//  Copyright © 2020 3pies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ApiRestClient.
+FOUNDATION_EXPORT double ApiRestClientVersionNumber;
+
+//! Project version string for ApiRestClient.
+FOUNDATION_EXPORT const unsigned char ApiRestClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ApiRestClient/PublicHeader.h>
+
+
